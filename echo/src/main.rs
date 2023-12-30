46afd311e2fd241cd5d566202a479311734ae695
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-30 19:37:33 +0900

    echo: 必須引数と-nフラグの追加

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,11 +1,24 @@
-use clap::Command;
+use clap::{Arg, ArgAction, Command};
 
 fn main() {
-  // アンダースコアはRustコンパイラーに、今すぐこの変数を使うつもりはないことを伝えるもの
-  // アンダースコアがないと、コンパイラーは未使用の変数であることを警告する
-  let _matches = Command::new("echor")
+  let matches = Command::new("echor")
     .version("0.1.0")
     .author("tomixy")
     .about("echo command by Rust")
+    .arg(
+      Arg::new("text")
+        .value_name("TEXT")
+        .help("Input text")
+        .required(true)
+        .num_args(1..),
+    )
+    .arg(
+      Arg::new("omit_newline")
+        .short('n')
+        .action(ArgAction::SetTrue)
+        .help("Do not print newline"),
+    )
     .get_matches();
+
+  println!("{:#?}", matches);
 }
