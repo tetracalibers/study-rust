02fbf4c30c144a82050655544bb5fbeb2d42030e
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 07:03:38 +0900

    echor: echoの出力とechorの出力が一致することのテスト

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 // 引数を指定しない場合、失敗してヘルプ・ドキュメントを表示する
 #[test]
@@ -14,4 +15,12 @@ fn dies_no_args() {
 fn runs() {
   let mut cmd = Command::cargo_bin("echor").unwrap();
   cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1() {
+  let outfile = "tests/expected/hello1.txt";
+  let expected = fs::read_to_string(outfile).unwrap();
+  let mut cmd = Command::cargo_bin("echor").unwrap();
+  cmd.arg("Hello there").assert().success().stdout(expected);
 }
