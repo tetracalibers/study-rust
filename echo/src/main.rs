53a8ace697c9568f8a4f7cda5927d30dc814762b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-30 19:30:12 +0900

    echo: -hフラグへの対応

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,11 @@
+use clap::Command;
+
 fn main() {
-  println!("Hello, world!");
+  // アンダースコアはRustコンパイラーに、今すぐこの変数を使うつもりはないことを伝えるもの
+  // アンダースコアがないと、コンパイラーは未使用の変数であることを警告する
+  let _matches = Command::new("echor")
+    .version("0.1.0")
+    .author("tomixy")
+    .about("echo command by Rust")
+    .get_matches();
 }
