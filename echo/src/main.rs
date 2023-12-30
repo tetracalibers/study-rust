f91102a31f7096e900ea48b84d67ffc8345485bf
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 06:43:44 +0900

    echo: Creating the Program Output

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -20,5 +20,10 @@ fn main() {
     )
     .get_matches();
 
-  println!("{:#?}", matches);
+  let text: Vec<String> =
+    matches.get_many("text").expect("text is required").cloned().collect();
+
+  let omit_newline = matches.get_flag("omit_newline");
+
+  print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
