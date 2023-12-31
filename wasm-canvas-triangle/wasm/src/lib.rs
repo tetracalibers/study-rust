1cbcff35a094f7d13039c0ead1d5d49dcde17b0d
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-01 06:45:12 +0900

    wasm-canvas-triangle: ブラウザconsoleでHello World

--- wasm-canvas-triangle/wasm/src/lib.rs
+++ wasm-canvas-triangle/wasm/src/lib.rs
@@ -1,13 +1,11 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+  console_error_panic_hook::set_once();
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+  console::log_1(&JsValue::from_str("Hello world!"));
+
+  Ok(())
 }
