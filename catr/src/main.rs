a238a27c10226bbcd42e1355b33388eaa298e39a
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 11:37:13 +0900

    catr: Creating a Library Crate

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-  println!("Hello, world!");
+  // catr::run関数を実行し、戻り値がErr(e)にマッチするかどうかをチェックする
+  if let Err(e) = catr::run() {
+    // eprintln!マクロを使って、エラーメッセージをSTDERRにプリント
+    eprintln!("{}", e);
+    // エラーを示す非ゼロ値でプログラムを終了
+    std::process::exit(1);
+  }
 }
