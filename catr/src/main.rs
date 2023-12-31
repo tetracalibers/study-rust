af561029b75acf130403443022798185c697d77f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 11:50:25 +0900

    catr: configを正常取得できたら、run関数に渡す

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
   // catr::run関数を実行し、戻り値がErr(e)にマッチするかどうかをチェックする
-  if let Err(e) = catr::run() {
+  if let Err(e) = catr::get_args().and_then(catr::run) {
     // eprintln!マクロを使って、エラーメッセージをSTDERRにプリント
     eprintln!("{}", e);
     // エラーを示す非ゼロ値でプログラムを終了
