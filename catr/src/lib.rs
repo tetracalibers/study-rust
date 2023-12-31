9963e5acab39060854e4b7fab29f48860dbc3586
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 11:43:11 +0900

    catr: Config構造体の定義

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,13 @@
 use std::error::Error;
 
+// deriveマクロは、構造体を出力できるようにDebug traitを追加する
+#[derive(Debug)]
+pub struct Config {
+  files: Vec<String>,
+  number_lines: bool, // 行番号を表示するかどうかを示すブール値
+  number_nonblank_lines: bool, // 空白行以外の行番号の印字を制御するブール値
+}
+
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run() -> MyResult<()> {
