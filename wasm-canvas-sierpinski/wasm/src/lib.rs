be47cf1ef6b28bff49caea52d5214094fd7df27f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-01 07:40:35 +0900

    wasm-canvas-sierpinski: 三角形の描画を関数にまとめる

--- wasm-canvas-sierpinski/wasm/src/lib.rs
+++ wasm-canvas-sierpinski/wasm/src/lib.rs
@@ -1,13 +1,41 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+  console_error_panic_hook::set_once();
+
+  let window = web_sys::window().unwrap();
+  let document = window.document().unwrap();
+
+  let canvas = document
+    .get_element_by_id("canvas")
+    .unwrap()
+    .dyn_into::<web_sys::HtmlCanvasElement>()
+    .unwrap();
+
+  let context = canvas
+    .get_context("2d")
+    .unwrap()
+    .unwrap()
+    .dyn_into::<web_sys::CanvasRenderingContext2d>()
+    .unwrap();
+
+  draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
+
+  Ok(())
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+fn draw_triangle(
+  context: &web_sys::CanvasRenderingContext2d,
+  points: [(f64, f64); 3],
+) {
+  let [top, left, right] = points;
+  context.move_to(top.0, top.1);
+  context.begin_path();
+  context.line_to(left.0, left.1);
+  context.line_to(right.0, right.1);
+  context.line_to(top.0, top.1);
+  context.close_path();
+  context.stroke();
 }
