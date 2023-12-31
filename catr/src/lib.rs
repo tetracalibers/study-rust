bbb8cfc5fe5d3c74d52e17c1957866b93dc940cf
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 11:48:10 +0900

    catr: パラメータ解析関数を実装

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,3 +1,4 @@
+use clap::{Arg, ArgAction, Command};
 use std::error::Error;
 
 // deriveマクロは、構造体を出力できるようにDebug traitを追加する
@@ -9,6 +10,46 @@ pub struct Config {
 }
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn get_args() -> MyResult<Config> {
+  let matches = Command::new("echor")
+    .version("0.1.0")
+    .author("tomixy")
+    .about("cat command by Rust")
+    .arg(
+      Arg::new("files")
+        .value_name("FILE")
+        .help("Input file(s)")
+        .num_args(1..)
+        .default_value("-"),
+    )
+    .arg(
+      Arg::new("number")
+        .short('n')
+        .long("number")
+        .help("Number lines")
+        .action(ArgAction::SetTrue)
+        .conflicts_with("number_nonblank"),
+    )
+    .arg(
+      Arg::new("number_nonblank")
+        .short('b')
+        .long("number-nonblank")
+        .help("Number non-blank lines")
+        .action(ArgAction::SetTrue),
+    )
+    .get_matches();
+
+  Ok(Config {
+    files: matches
+      .get_many("files")
+      .expect("files required")
+      .cloned()
+      .collect(),
+    number_lines: matches.get_flag("number"),
+    number_nonblank_lines: matches.get_flag("number_nonblank"),
+  })
+}
 
 pub fn run() -> MyResult<()> {
   println!("Hello, world!");
