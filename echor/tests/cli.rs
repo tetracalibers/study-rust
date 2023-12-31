6ae4ea5759cce2c3ff31a5fcc7b4f87f55516b35
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 11:09:27 +0900

    echor: 成功する前提でunwrapを使うのは危険なのでやめる

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -2,12 +2,15 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 // 引数を指定しない場合、失敗してヘルプ・ドキュメントを表示する
 #[test]
-fn dies_no_args() {
-  let mut cmd = Command::cargo_bin("echor").unwrap();
-  // 引数なしでプログラムを実行 => 失敗 => STDERRに使用法を表示
+fn dies_no_args() -> TestResult {
+  let mut cmd = Command::cargo_bin("echor")?;
   cmd.assert().failure().stderr(predicate::str::contains("Usage"));
+  // この値を返すには、最後のセミコロンを省略する
+  Ok(())
 }
 
 // 引数が与えられたときにプログラムが正常に終了する
@@ -18,9 +21,17 @@ fn runs() {
 }
 
 #[test]
-fn hello1() {
-  let outfile = "tests/expected/hello1.txt";
-  let expected = fs::read_to_string(outfile).unwrap();
-  let mut cmd = Command::cargo_bin("echor").unwrap();
+fn hello1() -> TestResult {
+  let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+  let mut cmd = Command::cargo_bin("echor")?;
   cmd.arg("Hello there").assert().success().stdout(expected);
+  Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+  let expected = fs::read_to_string("tests/expected/hello2.txt")?;
+  let mut cmd = Command::cargo_bin("echor")?;
+  cmd.args(vec!["Hello", "there"]).assert().success().stdout(expected);
+  Ok(())
 }
