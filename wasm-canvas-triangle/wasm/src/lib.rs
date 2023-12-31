400e85ad99789767f7fdeadf1eb0ac4fd92de28f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-01 07:15:59 +0900

    wasm-canvas-triangle: WASMでcanvasに黒い三角形を描画

--- wasm-canvas-triangle/wasm/src/lib.rs
+++ wasm-canvas-triangle/wasm/src/lib.rs
@@ -1,11 +1,34 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
-#[wasm_bindgen]
-extern "C" {
-  fn alert(s: &str);
-}
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+  console_error_panic_hook::set_once();
 
-#[wasm_bindgen]
-pub fn greet() {
-  alert("Hello, wasm!");
+  let window = web_sys::window().unwrap();
+  let document = window.document().unwrap();
+
+  let canvas = document
+    .get_element_by_id("canvas")
+    .unwrap()
+    .dyn_into::<web_sys::HtmlCanvasElement>()
+    .unwrap();
+
+  let context = canvas
+    .get_context("2d")
+    .unwrap()
+    .unwrap()
+    .dyn_into::<web_sys::CanvasRenderingContext2d>()
+    .unwrap();
+
+  context.move_to(300.0, 0.0); // top of triangle
+  context.begin_path();
+  context.line_to(0.0, 600.0); // bottom left of triangle
+  context.line_to(600.0, 600.0); // bottom right of triangle
+  context.line_to(300.0, 0.0); // back to top of triangle
+  context.close_path();
+  context.stroke();
+  context.fill();
+
+  Ok(())
 }
