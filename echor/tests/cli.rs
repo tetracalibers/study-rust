bddbf8addc5ffb6a6c4f10aa96eb2ce855c3085f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2023-12-31 11:17:53 +0900

    echor: ファイルによるテスト用のヘルパー関数runを用意

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -9,29 +9,36 @@ type TestResult = Result<(), Box<dyn std::error::Error>>;
 fn dies_no_args() -> TestResult {
   let mut cmd = Command::cargo_bin("echor")?;
   cmd.assert().failure().stderr(predicate::str::contains("Usage"));
+  // これまでのコードがすべて機能した場合は、ユニット・タイプを含むOkを返す
   // この値を返すには、最後のセミコロンを省略する
   Ok(())
 }
 
-// 引数が与えられたときにプログラムが正常に終了する
-#[test]
-fn runs() {
-  let mut cmd = Command::cargo_bin("echor").unwrap();
-  cmd.arg("hello").assert().success();
+// argsは&str値のスライス、expect_fileは&str、戻り値はTestResult
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+  // expected_fileの内容を文字列に読み込もうとする
+  let expected = fs::read_to_string(expected_file)?;
+  Command::cargo_bin("echor")?.args(args).assert().success().stdout(expected);
+  Ok(())
 }
 
 #[test]
 fn hello1() -> TestResult {
-  let expected = fs::read_to_string("tests/expected/hello1.txt")?;
-  let mut cmd = Command::cargo_bin("echor")?;
-  cmd.arg("Hello there").assert().success().stdout(expected);
-  Ok(())
+  // この関数は、run関数が返すものなら何でも返すので、終端のセミコロンがないことに注意
+  run(&["Hello there"], "tests/expected/hello1.txt")
 }
 
 #[test]
 fn hello2() -> TestResult {
-  let expected = fs::read_to_string("tests/expected/hello2.txt")?;
-  let mut cmd = Command::cargo_bin("echor")?;
-  cmd.args(vec!["Hello", "there"]).assert().success().stdout(expected);
-  Ok(())
+  run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+  run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+  run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
