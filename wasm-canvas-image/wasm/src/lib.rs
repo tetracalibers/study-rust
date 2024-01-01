496a077ab0f1b2251a68577373b52453b45194a7
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-01 14:42:01 +0900

    wasm-canvas-image: load image

--- wasm-canvas-image/wasm/src/lib.rs
+++ wasm-canvas-image/wasm/src/lib.rs
@@ -1,13 +1,27 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+  console_error_panic_hook::set_once();
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+  let window = web_sys::window().unwrap();
+  let document = window.document().unwrap();
+
+  let canvas = document
+    .get_element_by_id("canvas")
+    .unwrap()
+    .dyn_into::<web_sys::HtmlCanvasElement>()
+    .unwrap();
+
+  let context = canvas
+    .get_context("2d")
+    .unwrap()
+    .unwrap()
+    .dyn_into::<web_sys::CanvasRenderingContext2d>()
+    .unwrap();
+
+  let image = web_sys::HtmlImageElement::new().unwrap();
+  image.set_src("Idle/image_3.png");
+
+  Ok(())
 }
