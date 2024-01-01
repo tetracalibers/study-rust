4d1388bc8d76f6c777edff7fbe9a2038c4c5c73e
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-02 08:31:41 +0900

    guessing_game: 秘密の数字を生成する

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,14 @@
 // @see https://doc.rust-jp.rs/book-ja/ch02-00-guessing-game-tutorial.html
 
+use rand::Rng;
 use std::io;
 
 fn main() {
   println!("Guess the number!"); // 数を当ててごらん
+
+  let secret_number = rand::thread_rng().gen_range(1..101);
+
+  println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
 
   println!("Please input your guess."); // ほら、予想を入力してね
 
