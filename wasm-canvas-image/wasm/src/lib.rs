f0d4e65206c2f6577e512d534af4161d80b63bfb
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-01 14:55:59 +0900

    wasm-canvas-image: 画像ロード時のコールバックを設定

--- wasm-canvas-image/wasm/src/lib.rs
+++ wasm-canvas-image/wasm/src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -21,7 +22,21 @@ pub fn main_js() -> Result<(), JsValue> {
     .unwrap();
 
   let image = web_sys::HtmlImageElement::new().unwrap();
-  image.set_src("Idle/image_3.png");
+
+  // Closure は wasm-bindgen が提供する構造体で、RustのクロージャをJavaScriptに渡すためのもの
+  // onloadハンドラは一度しか呼ばれないので、Closure::onceを使う
+  let callback = Closure::once(|| {
+    web_sys::console::log_1(&JsValue::from_str("Image loaded!"));
+  });
+
+  // as_ref() は 生のJsValue を返す
+  // unchecked_ref() で &Functionオブジェクト に変換する
+  // 引数はJSではnullの可能性があるため、Someでラップする
+  image.set_onload(Some(callback.as_ref().unchecked_ref()));
+  
+  image.set_src("/Idle/image_3.png");
+
+  context.draw_image_with_html_image_element(&image, 0.0, 0.0);
 
   Ok(())
 }
