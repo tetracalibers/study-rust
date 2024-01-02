0ea62f3caf0c916781d8f7514e1249b2550fa124
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-03 06:40:49 +0900

    minigrep: panic!を呼び出す代わりにnewからResultを返す

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let config = Config::new(&args);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    // 引数解析時に問題
+    println!("Problem parsing arguments: {}", err);
+    process::exit(1);
+  });
 
   println!("Searching for {}", config.query);
   println!("In file {}", config.filename);
@@ -27,15 +32,15 @@ struct Config {
 }
 
 impl Config {
-  fn new(args: &[String]) -> Config {
+  // &'static strは文字列リテラルの型
+  fn new(args: &[String]) -> Result<Config, &'static str> {
     if args.len() < 3 {
-      // 引数の数が足りません
-      panic!("not enough arguments");
+      return Err("not enough arguments");
     }
 
     let query = args[1].clone();
     let filename = args[2].clone();
 
-    Config { query, filename }
+    Ok(Config { query, filename })
   }
 }
