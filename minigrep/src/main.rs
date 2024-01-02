eab90e91dbab6ddf8e622f260635d1db512a8080
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-03 06:27:40 +0900

    minigrep: 第2引数で指定されたファイルの中身を読み込む

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -10,4 +12,15 @@ fn main() {
   println!("Searching for {}", query);
   // {}というファイルの中
   println!("In file {}", filename);
+
+  // ファイルが見つかりませんでした
+  let mut f = File::open(filename).expect("file not found");
+
+  let mut contents = String::new();
+  f.read_to_string(&mut contents)
+    // ファイルの読み込み中に問題がありました
+    .expect("something went wrong reading the file");
+
+  // テキストは\n{}です
+  println!("With text:\n{}", contents);
 }
