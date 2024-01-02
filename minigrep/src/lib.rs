97ff701c5314fedbe1d085711c5bb7782c051961
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-03 07:49:46 +0900

    minigrep: イテレータを使用してcloneを取り除く

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -11,13 +11,19 @@ pub struct Config {
 
 impl Config {
   // &'static strは文字列リテラルの型
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
+  pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+    args.next();
 
-    let query = args[1].clone();
-    let filename = args[2].clone();
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a query string"),
+    };
+
+    let filename = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Didn't get a file name"),
+    };
+
     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
     Ok(Config {
