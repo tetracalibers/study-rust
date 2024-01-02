ca134d0f3b9bb51d580f0decd25f140afdd25150
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-03 06:25:10 +0900

    minigrep: クエリ引数とファイル名引数を保持する変数を生成

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  println!("{:?}", args);
+
+  let query = &args[1];
+  let filename = &args[2];
+
+  // {}を探しています
+  println!("Searching for {}", query);
+  // {}というファイルの中
+  println!("In file {}", filename);
 }
