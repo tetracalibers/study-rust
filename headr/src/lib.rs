91f86a2397115dd17179cff5d6a8ed5490ac9745
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-04 08:47:05 +0900

    headr: Processing the Input Files

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{Arg, Command};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -50,7 +52,12 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-  dbg!(config);
+  for filename in config.files {
+    match open(&filename) {
+      Err(err) => eprintln!("{}: {}", filename, err),
+      Ok(_) => println!("Opened {}", filename),
+    }
+  }
   Ok(())
 }
 
@@ -58,6 +65,13 @@ fn parse_positive_int(val: &str) -> MyResult<usize> {
   match val.parse() {
     Ok(n) if n > 0 => Ok(n),
     _ => Err(From::from(val)),
+  }
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+  match filename {
+    "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+    _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
   }
 }
 
