4ac37851aea71650da032377f2f1a3548edc8bcb
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-05 09:49:08 +0900

    wcr: File Elements定義、count関数の雛形とテストを用意

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -14,6 +14,14 @@ pub struct Config {
   chars: bool,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+  num_lines: usize,
+  num_words: usize,
+  num_bytes: usize,
+  num_chars: usize,
+}
+
 pub fn run(config: Config) -> MyResult<()> {
   for filename in &config.files {
     match open(filename) {
@@ -100,5 +108,39 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
   match filename {
     "-" => Ok(Box::new(BufReader::new(io::stdin()))),
     _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+  }
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+  let mut num_lines = 0;
+  let mut num_words = 0;
+  let mut num_bytes = 0;
+  let mut num_chars = 0;
+
+  Ok(FileInfo {
+    num_lines,
+    num_words,
+    num_bytes,
+    num_chars,
+  })
+}
+
+#[cfg(test)]
+mod tests {
+  use super::{count, FileInfo};
+  use std::io::Cursor;
+
+  #[test]
+  fn test_count() {
+    let text = "I don't want the world. I just want your half.\r\n";
+    let info = count(Cursor::new(text));
+    assert!(info.is_ok());
+    let expected = FileInfo {
+      num_lines: 1,
+      num_words: 10,
+      num_chars: 48,
+      num_bytes: 48,
+    };
+    assert_eq!(info.unwrap(), expected);
   }
 }
