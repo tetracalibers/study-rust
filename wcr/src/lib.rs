baae53e523589ce2c9f488aeb68c579c3c2d4fdc
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-01-05 09:17:48 +0900

    wcr: 引数の解析

---
+++ wcr/src/lib.rs
@@ -0,0 +1,89 @@
+use clap::{Arg, ArgAction, Command};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+  files: Vec<String>,
+  lines: bool,
+  words: bool,
+  bytes: bool,
+  chars: bool,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+  println!("{:#?}", config);
+  Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+  let matches = Command::new("wcr")
+    .version("0.1.0")
+    .author("tomixy")
+    .about("wc command by Rust")
+    .arg(
+      Arg::new("files")
+        .value_name("FILE")
+        .help("Input file(s)")
+        .num_args(1..)
+        .default_value("-"),
+    )
+    .arg(
+      Arg::new("bytes")
+        .short('c')
+        .long("bytes")
+        .value_name("BYTES")
+        .help("Show byte count")
+        .action(ArgAction::SetTrue),
+    )
+    .arg(
+      Arg::new("chars")
+        .short('m')
+        .long("chars")
+        .value_name("CHARS")
+        .help("Show character count")
+        .action(ArgAction::SetTrue)
+        .conflicts_with("bytes"),
+    )
+    .arg(
+      Arg::new("lines")
+        .short('l')
+        .long("lines")
+        .value_name("LINES")
+        .help("Show line count")
+        .action(ArgAction::SetTrue),
+    )
+    .arg(
+      Arg::new("words")
+        .short('w')
+        .long("words")
+        .value_name("WORDS")
+        .help("Show word count")
+        .action(ArgAction::SetTrue),
+    )
+    .get_matches();
+
+  let mut lines = matches.get_flag("lines");
+  let mut words = matches.get_flag("words");
+  let mut bytes = matches.get_flag("bytes");
+  let chars = matches.get_flag("chars");
+
+  if [words, bytes, chars, lines].iter().all(|v| v == &false) {
+    lines = true;
+    words = true;
+    bytes = true;
+  }
+
+  Ok(Config {
+    files: matches
+      .get_many("files")
+      .expect("files required")
+      .cloned()
+      .collect(),
+    lines,
+    words,
+    bytes,
+    chars,
+  })
+}
